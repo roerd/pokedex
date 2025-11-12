cdee81a7f3456925334448dce901a88c418ff07c
Rörd Hinrichsen <roerdhh@gmail.com>
2025-11-12 07:59:37 +0100

    Implement basic REPL

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
+// repl.js actually refers to repl.ts
+import { startREPL, cleanInput } from "./repl.js";
+
 function main() {
-  console.log("Hello, world!");
+  const repl = startREPL();
+  repl.prompt();
+
+  repl.on('line', (line: string) => {
+    const words = cleanInput(line);
+    if (words.length === 0) {
+      repl.prompt();
+      return;
+    }
+    
+    const command = words[0].toLowerCase();
+
+    console.log(`Your command was: ${command}\n`);
+    repl.prompt();
+  });
 }
 
 main();
--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,13 @@
+import { createInterface } from 'node:readline';
+
 export function cleanInput(input: string): string[] {
     return input.split(/\s+/).filter(s => s.length > 0);
+}
+
+export function startREPL() {
+  return createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Pokedex > '
+    });
 }
