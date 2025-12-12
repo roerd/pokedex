5eeeb9c0bc0939315e7d76749a31a85fe0017025
Rörd Hinrichsen <roerdhh@gmail.com>
2025-12-12 22:17:34 +0100

    Implement command mapb

---
+++ src/command_mapb.ts
@@ -0,0 +1,15 @@
+import type { State } from "./state.js";
+
+export async function commandMapB(state: State) {
+    if (!state.pokeapi.prevLocationsURL) {
+        console.log("you're on the first page");
+        return state;
+    }
+    const locations = await state.pokeapi.fetchLocations(state.pokeapi.prevLocationsURL);
+
+    for (const location of locations.results) {
+        console.log(location.name);
+    }
+
+    return state;
+}
