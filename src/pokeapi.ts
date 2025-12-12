1f12973679a9eae3b0189dae2c3f90308ea39fe0
Rörd Hinrichsen <roerdhh@gmail.com>
2025-12-12 18:23:42 +0100

    Implement class PokeAPI

---
+++ src/pokeapi.ts
@@ -0,0 +1,54 @@
+export class PokeAPI {
+  private static readonly baseURL = "https://pokeapi.co/api/v2";
+  public previous = null;
+  public next = null;
+
+  constructor() {}
+
+  async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
+    const result = await fetch(
+      pageURL ? pageURL : `${PokeAPI.baseURL}/location-area/`
+    );
+    const data = await result.json();
+    this.previous = data.previous;
+    this.next = data.next;
+    return data;
+  }
+
+  async fetchLocation(locationName: string): Promise<Location> {
+    const result = await fetch(
+      `${PokeAPI.baseURL}/location-area/${locationName}/`
+    );
+    const data = await result.json();
+    return data;
+  }
+}
+
+export type ShallowLocations = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Array<{
+    name: string;
+    url: string;
+  }>;
+};
+
+export type Location = {
+  id: number;
+  name: string;
+  game_index: number;
+  encounter_method_rates: Array<unknown>;
+  location: {
+    name: string;
+    url: string;
+  };
+  names: Array<{
+    name: string;
+    language: {
+      name: string;
+      url: string;
+    };
+  }>;
+  pokemon_encounters: Array<unknown>;
+};
