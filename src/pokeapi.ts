ffbcf4728e6804aecc1f93ddf974de11457c7f89
Rörd Hinrichsen <roerdhh@gmail.com>
2025-12-13 22:56:31 +0100

    Update PokeAPI to use Cache

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -1,14 +1,19 @@
+import { Cache } from "./pokecache.js";
+
 export class PokeAPI {
   private static readonly baseURL = "https://pokeapi.co/api/v2";
   public prevLocationsURL = undefined;
   public nextLocationsURL = undefined;
+  private cache = new Cache(60000);
 
   constructor() {}
 
   async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
-    const result = await fetch(
-      pageURL ?? `${PokeAPI.baseURL}/location-area/`
-    );
+    const url = pageURL ?? `${PokeAPI.baseURL}/location-area/`;
+    if (this.cache.get<ShallowLocations>(url)) {
+      return this.cache.get<ShallowLocations>(url)!;
+    }
+    const result = await fetch(url);
     const data = await result.json();
     this.prevLocationsURL = data.previous ?? undefined;
     this.nextLocationsURL = data.next ?? undefined;
@@ -16,6 +21,9 @@ export class PokeAPI {
   }
 
   async fetchLocation(locationName: string): Promise<Location> {
+    if (this.cache.get<Location>(locationName)) {
+      return this.cache.get<Location>(locationName)!;
+    }
     const result = await fetch(
       `${PokeAPI.baseURL}/location-area/${locationName}/`
     );
