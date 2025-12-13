a4ddf92994b9234fc2b1d16908e5f708f755c74d
Rörd Hinrichsen <roerdhh@gmail.com>
2025-12-13 22:49:30 +0100

    Implement reap loop for Cache

--- src/pokecache.ts
+++ src/pokecache.ts
@@ -4,17 +4,44 @@ export type CacheEntry<T> = {
 };
 
 export class Cache {
-  #cache = new Map<string, CacheEntry<any>>();
+    #cache = new Map<string, CacheEntry<any>>();
+    #reapIntervalId: NodeJS.Timeout | undefined = undefined;
+    #interval: number;
 
-  add<T>(key: string, val: T) {
-    const entry: CacheEntry<T> = {
-      createdAt: Date.now(),
-      val,
-    };
-    this.#cache.set(key, entry);
-  }
+    constructor(reapIntervalMs: number = 60000) {
+        this.#interval = reapIntervalMs;
+        this.#startReapLoop();
+    }
+
+    #startReapLoop() {
+        this.#reapIntervalId = setInterval(this.#reap, this.#interval);
+    }
+
+    #reap() {
+        const cutOff = Date.now() - this.#interval;
+        for (const [key, entry] of this.#cache.entries()) {
+            if (entry.createdAt < cutOff) {
+                this.#cache.delete(key);
+            }
+        }
+    }
+
+    stopReapLoop() {
+        if (this.#reapIntervalId) {
+            clearInterval(this.#reapIntervalId);
+            this.#reapIntervalId = undefined;
+        }
+    }
+
+    add<T>(key: string, val: T) { 
+        const entry: CacheEntry<T> = {
+            createdAt: Date.now(),
+            val,
+        };
+        this.#cache.set(key, entry);
+    }
   
-  get<T>(key: string): T | undefined {
-    return this.#cache.get(key)?.val;
-  }
+    get<T>(key: string): T | undefined {
+        return this.#cache.get(key)?.val;
+    }
 }
